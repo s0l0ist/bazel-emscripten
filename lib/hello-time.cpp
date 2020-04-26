d4b5c518a5f6d783fbf9c3f98151facd2d56ba2a
Nick Angelou <nick@morfix.io>
2020-04-26 14:33:22 -0500

    fix: use printf

--- lib/hello-time.cpp
+++ lib/hello-time.cpp
@@ -1,8 +1,10 @@
 #include "lib/hello-time.hpp"
 #include <ctime>
-#include <iostream>
+#include <stdio.h>
+// #include <iostream>
 
 void print_localtime() {
   std::time_t result = std::time(nullptr);
-  std::cout << std::asctime(std::localtime(&result));
+  // std::cout << std::asctime(std::localtime(&result));
+  printf("%s\n", std::asctime(std::localtime(&result)));
 }
--- main/hello-world.cpp
+++ main/hello-world.cpp
@@ -1,11 +1,14 @@
 #include "lib/hello-time.hpp"
 #include "main/hello-greet.hpp"
-#include <iostream>
+// #include <iostream>
+# include <stdio.h>
 #include <string>
 
 int main() {
   std::string who = "world";
-  std::cout << get_greet(who) << std::endl;
+  // std::cout << get_greet(who) << std::endl;
+  std::string greet = get_greet(who);
+  printf("%s\n", greet.c_str());
   print_localtime();
   return 0;
 }
