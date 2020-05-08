f33bc0abc39cbb676aad49e3e564e94885bcac83
Nick Angelou <nick@morfix.io>
2020-05-08 12:46:53 -0500

    fix: opt flags, demo code

--- hello-world/javascript/examples/demo.js
+++ hello-world/javascript/examples/demo.js
@@ -1,7 +1,12 @@
-const cpp_main = require('../src');
+const Library = require(`../dist/${process.env.RUN_DEMO}/umd`);
 
 (async () => {
-    // Wait for the initializer to complete loading
-    // Upon success, this will immediatly run our C++ main() function
-    await cpp_main(process.env.RUN_DEMO) // RUN_DEMO = js|wasm
+  // Wait for the library to initialize
+  const HelloWorld = await Library();
+
+  // Prints the current time to the console
+  HelloWorld.LocalTime.now();
+
+  // Returns a greeting for us to print
+  console.log(HelloWorld.Greet.sayHello());
 })();
